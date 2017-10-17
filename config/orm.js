717a937869388adae2a9c638e9f82ec42ef3c5c6
vGknight <gregory.knight@outlook.com>
2017-10-16 23:31:18 -0400

    testing sql

--- config/orm.js
+++ config/orm.js
@@ -14,24 +14,25 @@ console.log("orm.js loaded");
 
 // updateOne()
 
-//     cnx.query('SELECT id,burgers_name,devoured,date FROM `actors`', function(error, results, fields) {
-//         if (error) throw error;
+    cnx.query('SELECT id,burgers_name,devoured,date FROM `burgers`', function(error, results, fields) {
+        if (error) throw error;
 
-//         for (var i = 0; i < results.length; i++) {
+        for (var i = 0; i < results.length; i++) {
 
-//             // idArr.push(results[i].item_id); // add id's to control variable array for validation later
-//             console.log(" ");
-//             console.log("ID: " + results[i].id + "Name: " + results[i].burgers_name + " | Devoured: " + results[i].devoured + " | TimeStamp: " + results[i].date;
-//             console.log(" ");
+            // idArr.push(results[i].item_id); // add id's to control variable array for validation later
+            console.log(" ");
+            console.log("ID: " + results[i].id + "Name: " + results[i].burgers_name + " | Devoured: " + results[i].devoured + " | TimeStamp: " + results[i].date);
+            console.log(" ");
+
 
 //             //build html
-//         }
+        }
 //         // cnx.end();
 
 
 //         // res.json(results);
 
 
-//     });
+    });
 
 // module.exports = orm;
