8b69b4a3aff06a93695d82e75380f92dd63f95fd
unknown <gregory.knight@outlook.com>
2017-10-22 16:26:39 -0400

    updated to use env var's

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -2,59 +2,52 @@
 $(function() {
 
 
-  $(".change-devoured").on("click", function(event) {
-    var id = $(this).data("id");
+    $(".change-devoured").on("click", function(event) {
+        var id = $(this).data("id");
 
-    var devoured = {
-      devoured: 1
-    };
+        var devoured = {
+            devoured: 1
+        };
 
-    // Send the PUT request.
-    $.ajax("/api/update/" + id, {
-      type: "PUT",
-      data: devoured
-    }).then(
-      function() {
-        console.log("Burger is now eaten!", devoured);
-        // Reload the page to get the updated list
-        location.reload();
-        // $(".burgers-eaten").load(location.href + " .burgers-eaten"); 
-        // $("#burgers-eaten").load("#burgers-eaten");
-
-
-
-      }
-    );
-  });
-
-
-  $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
-
-    event.preventDefault();
-
-    var newBurger = {
-      burgers_name: $("#burger").val().trim(),
-      devoured: 0
-
-    };
-
-    // Send the POST request.
-    $.ajax("/api/add", {
-      type: "POST",
-      data: newBurger
-    }).then(
-      function() {
-        console.log("Burger Added...");
-        // Reload the page to get the updated list - this sucks
-        location.reload();
-        
-        // $("#burgers-uneaten").load("#burgers-uneaten");
+        // Send the PUT request.
+        $.ajax("/api/update/" + id, {
+            type: "PUT",
+            data: devoured
+        }).then(
+            function() {
+                console.log("Burger is now eaten!", devoured);
+                // Reload the page to get the updated list
+                location.reload();
    
-      }
-    );
-  });
+            }
+        );
+    });
+
+
+    $(".create-form").on("submit", function(event) {
+        // Make sure to preventDefault on a submit event.
+
+        event.preventDefault();
+
+        var newBurger = {
+            burgers_name: $("#burger").val().trim(),
+            devoured: 0
+
+        };
+
+        // Send the POST request.
+        $.ajax("/api/add", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                console.log("Burger Added...");
+                // Reload the page to get the updated list - this sucks
+                location.reload();
+
+
+            }
+        );
+    });
 
 });
-
-
